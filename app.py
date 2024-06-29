3d885f4f2da830fb0da68fc5251501229b7532be
Melene <melenetonou@gmail.com>
2024-06-29 21:08:38 +0200

    Initial commit

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 # app.py
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
-from config import MONGO_URI
+from config import Config
 from routes.auth.auth import authentification
 from utils.collections import Medecin_collection, Patient_collection, Alertes, Discussions
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = MONGO_URI
+app.config.from_object(Config)
 mongo = PyMongo(app)
+
+print("MONGO_URI:", app.config['MONGO_URI'])
+print("Mongo DB:", mongo.db)
 
 # Route d'accueil pour vérifier que tout fonctionne
 @app.route('/')
--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-# config.py
-MONGO_URI = "mongodb://localhost:27017/Medical"
+import os
+
+class Config:
+    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb+srv://melenetonou:mongodatabasepassword@sauvemoi.h15f2zf.mongodb.net/SauveMoi?retryWrites=true&w=majority'
